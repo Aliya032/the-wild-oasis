9f46de161632ca265c64905f2d2a94cedf65b634
Aliya <aliyahassan032@gmail.com>
2026-01-15 23:47:25 +0530

    changed files

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import { ESLint } from 'eslint'
+// import { ESLint } from "eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
